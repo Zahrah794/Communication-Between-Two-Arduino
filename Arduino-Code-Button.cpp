b802e877bcf5465c867240bc4092dc98971e1b97
Zahrah794 <139267881+Zahrah794@users.noreply.github.com>
2023-08-07 23:12:20 +0300

    Create Arduino-Code-Button.cpp

---
+++ Arduino-Code-Button.cpp
@@ -0,0 +1,22 @@
+#Arduino Button Control: Interfacing a Button for Interactive Functionality
+
+
+int flag_led = 0;       // Variable to store the LED flag status
+#define Buttonpin 7     // Pin number for the button
+
+void setup() {
+  Serial.begin(9600);   // Initialize the serial communication
+  pinMode(Buttonpin, INPUT);  // Set the button pin as input
+}
+
+void loop() {
+  // Check if the button is pressed
+  if (digitalRead(Buttonpin)) {
+    flag_led = 1;     // Set the LED flag to 1 if the button is pressed
+  } else {
+    flag_led = 0;     // Set the LED flag to 0 if the button is not pressed
+  }
+  
+  Serial.print(flag_led);   // Print the LED flag status
+  delay(1000);              // Delay for 1 second
+}
